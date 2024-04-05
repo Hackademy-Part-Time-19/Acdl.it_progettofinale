593fb2e8a95cbdb1c5932bf8ec2c153be617d234
claudia <claudia.matei@libero.it>
2024-04-05 18:53:45 +0200

    y

---
+++ lang/en/messages.php
@@ -0,0 +1,30 @@
+<?php
+return [
+  'Categorie' => 'categories',
+  'Benvenuto' => 'Welcome',
+  'InserisciAnnuncio' => 'Add advertisement',
+  'AreaRevisore' => "Reviewer's area",
+  'Logout' => 'Logout',
+  'Registrazione' => 'Register',
+  'Login' => 'Login',
+  'Cerca' => 'Search',
+  'Lavora' => 'Work with us.',
+  'DiventaRevisore' => 'Become a reviewer.',
+  'Candidatura' => 'Submit your application',
+  'Sviluppo' => 'Developed by aclm.it',
+  'Introduzione' => 'Welcome to Presto.it, register now and post your ad.',
+  'AnnunciHome' => 'Here are our announcements',
+  'Dettagli' => 'details',
+  'TitoloAnnuncio' => 'Title',
+  'Prezzo' => 'Price',
+  'Descrizione' => 'Description',
+  'Immagine' => 'Image',
+  'InserimentoCategoria' => 'Category',
+  'SelezionaCategoria' => 'Select the category',
+  'Salva' => 'Save',
+  'AnnuncioSuccesso' => 'Ad placed successfully',
+
+
+  'EsploraCategoria' => 'explore the category',
+  'VisualizzaDettagli' => 'show details',
+];
