acecbd75d6b1ce9846510fb9d2e4bcd925865a7f
mattiacursi <mattiacursi8@gmail.comgit pushgit config --global user.email mattiacursi8@gmail.comgit initialgit ush>
2024-04-10 21:00:45 +0200

    user story 8

--- app/Livewire/CreateAds.php
+++ app/Livewire/CreateAds.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Jobs\RemoveFaces;
 use App\Models\Ad;
 use Livewire\Component;
 use App\Models\Category;
@@ -76,7 +77,12 @@ class CreateAds extends Component
                 //$this->ad->images()->create(['path' => $image->store('images', 'public')]);
                 $newImage = $this->ad->images()->create(['path' => $image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 300, 300));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path , 300 , 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
+                
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
